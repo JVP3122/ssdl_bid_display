513fffe5c298db78a9bb936f29ec7bc980278880
Jeffrey.Pohlmeyer <jeffrey.pohlmeyer@nj.ambest.com>
2019-02-13 14:47:30 -0500

    Created python scripts to create proboards raw code for displaying winners as well as created JSON to display total bids for each player.

---
+++ ssdl_bid_display/src/components/Bids.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    {{ bids }}
+    <ul v-for="team in bids">
+      <li v-for="player in team">
+        {{ player }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Bids",
+    data() {
+      return {
+        bids: {},
+        items: [],
+        headers: [],
+      }
+    },
+    methods: {
+      getBids() {
+        this.bids = require('@/assets/bids/test/temp_bids.json');
+      },
+      parseBids() {
+        Object.keys(this.bids).forEach(bid => {
+          this.bids[bid] = JSON.parse(this.bids[bid])
+          console.log('bid', bid)
+          console.log('this.bids[bid]', this.bids[bid])
+        })
+      },
+    },
+    mounted() {
+      this.getBids();
+      this.parseBids();
+      console.log('this.bids', this.bids)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
